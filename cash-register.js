b949d11c4a0a8329b05f04901860158ae7611a46
Tyler <tyler.mackay@barclays.com>
2023-02-03 15:40:16 +0000

    Currencies add/test to see if push works

--- cash-register.js
+++ cash-register.js
@@ -2,7 +2,17 @@
 function cashRegister(price, cash, cid) {
     let status = ``;
     let change = [];
-
+const currencies = [
+    { name: 'ONE HUNDRED', value: 100.00},
+    { name: 'TWENTY', value: 20.00},
+    { name: 'TEN', value: 10.00},
+    { name: 'FIVE', value: 5.00},
+    { name: 'ONE', value: 1.00},
+    { name: 'QUARTER', value: 0.25},
+    { name: 'DIME', value: 0.10},
+    { name: 'NICKEL', value: 0.05},
+    { name: 'PENNY', value: 0.01}
+  ];
 
 
 
