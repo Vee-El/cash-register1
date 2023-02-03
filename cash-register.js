3803d8ed74c235d6f76ad599cc45398aef26a312
vee-el <veronicalennon@live.com>
2023-02-03 18:25:00 +0000

    theory of possible solution

--- cash-register.js
+++ cash-register.js
@@ -17,14 +17,64 @@ function cashRegister(price, cash, cid) {
     
 
 
-    
+    // counts the total cash in drawer 
     for (let i = 0; i < cid.length; i++) {
         let value = cid[i][1];
         cidTotal += value;
         cidTotal = Math.round(cidTotal*100)/100;
     }   
+    // delete
+    console.log(`cidTotal: ${cidTotal}`);
 
-    console.log(cidTotal);
+    // sums cash minus price
+    let changeDue = cash - price
+    // delete
+    console.log(`changeDue: ${changeDue}`);
+
+    // status: "INCORRECT_PAYMENT", change: []  
+    if (cash < price) {
+        status = "INCORRECT_PAYMENT"; 
+        // delete
+        console.log(`status: "INCORRECT_PAYMENT", change: []`);
+    }
+
+    // status: "INSUFFICIENT_FUNDS", change: []
+    if (cidTotal < changeDue) {
+        status = "INSUFFICIENT_FUNDS";
+        // delete
+        console.log(`status: "INSUFFICIENT_FUNDS", change: []`);
+    }
+  
+    /* status: "CLOSED", change: [...]
+    with cash-in-drawer as the value for the key change if it is equal to the change due. 
+    Include each currency unit in the drawer, even if its value is zero. 
+    (i.e. DO display ["NICKEL", 0]) */
+    if (cidTotal === changeDue) {
+        status = "CLOSED";
+        // returns PENNY,0.5,NICKEL,0... and not ["PENNY", 0.5], ["NICKEL", 0],...
+        change = cid; 
+        // delete
+        console.log(`status: ${status}, change: ${change}`)
+    }    
+
+    /* status: "OPEN", change: [...]
+    with the change due in coins and bills, as the value of the change key. Only include the 
+    value of a currency unit if its value is not zero. (i.e. do NOT display ["NICKEL", 0])
+    */
+    if (cidTotal > changeDue) {
+        /* create loop working from the end of the 'cid' array - reverse()
+        checking if the value is less than change due and more than $0.00 - cid[j][1]
+        if it is then deduct the value from 'changeDue' and add it to the 'change' array - splice()
+        */
+
+    }  
+    
+
+    
+
+
+
+
 
 
 
